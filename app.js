d8af97c939fa001d4b37a031d5a566d1c5ce3229
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-09 18:02:51 +0530

    comment add to app.js and code refactor

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
+// Importing required modules
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
 
 let blogPosts = [];
 
+// Seting up app.js
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Render Blogpost on homepage
+
 app.get("/", function (req, res) {
   res.render("home", {
-    homeContent: homeStartingContent,
     blogPosts: blogPosts,
   });
 });
 
+// Render Contact Page
+
 app.get("/contact", function (req, res) {
-  res.render("contact", { contact: contactContent });
+  res.render("contact");
 });
 
+// Render About Page
+
 app.get("/about", function (req, res) {
-  res.render("about", { about: aboutContent });
+  res.render("about");
 });
+
+// Render Compose Page
 
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
+
+// Add blogPost to Database
 
 app.post("/compose", function (req, res) {
   const blogPost = {
@@ -45,6 +49,8 @@ app.post("/compose", function (req, res) {
   blogPosts.push(blogPost);
   res.redirect("/");
 });
+
+// Render blogPost pages
 
 app.get("/posts/:postName", function (req, res) {
   const urlEntered = _.lowerCase(req.params.postName);
@@ -62,6 +68,8 @@ app.get("/posts/:postName", function (req, res) {
     res.render("404");
   }
 });
+
+// Listening for HTTPs request on the specific port
 
 const port = 3000;
 
